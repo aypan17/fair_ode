080b818952696c8cad3997e341825d599d9c7551
aypan <aypan@caltech.edu>
2020-08-21 19:55:58 -0700

    changes to beam eval

--- src/evaluator.py
+++ src/evaluator.py
@@ -282,19 +282,39 @@ class Evaluator(object):
 
         for (x1, len1), (x2, len2), nb_ops in iterator:
 
-            # target words to predict
-            alen = torch.arange(len2.max(), dtype=torch.long, device=len2.device)
-            pred_mask = alen[:, None] < len2[None] - 1  # do not predict anything given the last target word
-            y = x2[1:].masked_select(pred_mask[:-1])
-            assert len(y) == (len2 - 1).sum().item()
+            # Use TreeLSTM encoder
+            if params.treelstm:
+                # encode and get valid equation/solutions (those with max(int) < 2^self.num_bit)
+                encoded, len1, valid = encoder(x=xword, causal=False)
+                
+                x2, len2, encoded, len1 = to_cuda(torch.transpose(torch.transpose(x2,0,1)[valid],-1,0), len2[valid], encoded, len1)
+                nb_ops = nb_ops[valid]
+                x2 = x2[:len2.max().item(), :]
 
-            # cuda
-            x1, len1, x2, len2, y = to_cuda(x1, len1, x2, len2, y)
+                # target words to predict
+                alen = torch.arange(len2.max(), dtype=torch.long, device=len2.device)
+                pred_mask = alen[:, None] < len2[None] - 1  # do not predict anything given the last target word
+                y = x2[1:].masked_select(pred_mask[:-1])
+                y, _ = to_cuda(y, None)
+                assert len(y) == (len2 - 1).sum().item()
+
+                # decode / loss
+                decoded = decoder('fwd', x=x2, lengths=len2, causal=True, src_enc=encoded, src_len=len1)
+            # Use Transformer encoder
+            else:
+                # target words to predict
+                alen = torch.arange(len2.max(), dtype=torch.long, device=len2.device)
+                pred_mask = alen[:, None] < len2[None] - 1  # do not predict anything given the last target word
+                y = x2[1:].masked_select(pred_mask[:-1])
+                assert len(y) == (len2 - 1).sum().item()
+
+                x1, len1, x2, len2, y = to_cuda(x1, len1, x2, len2, y)
+
+                # forward / loss
+                encoded = encoder('fwd', x=x1, lengths=len1, causal=False)
+                decoded = decoder('fwd', x=x2, lengths=len2, causal=True, src_enc=encoded.transpose(0, 1), src_len=len1)
+
             bs = len(len1)
-
-            # forward
-            encoded = encoder('fwd', x=x1, lengths=len1, causal=False)
-            decoded = decoder('fwd', x=x2, lengths=len2, causal=True, src_enc=encoded.transpose(0, 1), src_len=len1)
             word_scores, loss = decoder('predict', tensor=decoded, pred_mask=pred_mask, y=y, get_scores=True)
 
             # correct outputs per sequence / valid top-1 predictions
@@ -326,14 +346,24 @@ class Evaluator(object):
             logger.info(f"({n_total.sum().item()}/{eval_size}) Found {bs - len(invalid_idx)}/{bs} valid top-1 predictions. Generating solutions ...")
 
             # generate
-            _, _, generations = decoder.generate_beam(
-                encoded.transpose(0, 1),
-                len1,
-                beam_size=params.beam_size,
-                length_penalty=params.beam_length_penalty,
-                early_stopping=params.beam_early_stopping,
-                max_len=params.max_len
-            )
+            if params.treelstm:
+                _, _, generations = decoder.generate_beam(
+                    encoded,
+                    len1,
+                    beam_size=params.beam_size,
+                    length_penalty=params.beam_length_penalty,
+                    early_stopping=params.beam_early_stopping,
+                    max_len=params.max_len
+                )
+            else:
+                _, _, generations = decoder.generate_beam(
+                    encoded.transpose(0, 1),
+                    len1,
+                    beam_size=params.beam_size,
+                    length_penalty=params.beam_length_penalty,
+                    early_stopping=params.beam_early_stopping,
+                    max_len=params.max_len
+                )
 
             # prepare inputs / hypotheses to check
             # if eval_verbose < 2, no beam search on equations solved greedily

132e6cfaadafa1bdd4059406376020bf2508b40b
aypan <aypan@caltech.edu>
2021-01-26 10:41:18 -0800

    added final scripts

---
+++ baseline_prod.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+export NGPU=8; python -m torch.distributed.launch --nproc_per_node=$NGPU main.py \
+	--exp_name baseline_productivity \
+	--baseline \
+	--n_dec_layers 8 \
+	--n_enc_layers 8 \
+	--n_heads 8 \
+	--dropout 0.3 \
+	--attention_dropout 0.1 \
+	--optimizer "adam,lr=0.0001" \
+	--emb_dim 512 \
+	--batch_size 32 \
+	--tasks "prim_fwd" \
+	--reload_precomputed_data "prim_fwd,data_productivity/fwd_2to8_train,data_productivity/fwd_9to15_valid,data_productivity/fwd_9to16_test" \
+	--reload_size 10000000 \
+	--epoch_size 125000 \
+	--max_epoch 200  
+echo done
